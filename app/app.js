a71637c80406af5e758025c729cd868a35d2574b
Stephen Piccott <stevepiccott@gmail.com>
2015-08-19 22:01:30 -0230

    Adds further styling, removes urls with no file extension

--- app/app.js
+++ app/app.js
@@ -8,20 +8,20 @@ $(document).ready(function(){
 });
 
 function getRequest(searchTerm){
-
-	$.getJSON("http://www.reddit.com/r/" + searchTerm + "/.json?jsonp=?", function(data) { 
+	$.getJSON("http://www.reddit.com/r/" + searchTerm + "/.json?limit=100", function(data) { 
 		$.each(data.data.children, function(i,item){
 			img = item.data.url;
-			console.log(item.data);
 			if (img.match(/\.(jpg|jpeg|png|gif)$/)){
 				$('#images').append('<a href="' + img + '"><div class="image"><img class="fill-box" src="' + img + '"/></div></a>');
 				$(".fill-box").fillBox();
 			}
-			else if (img.search("imgur") != -1) {
-				img = img+".jpg"
-				$('#images').append('<a href="' + img + '"><div class="image"><img class="fill-box" src="' + img + '"/></div></a>');
-				$(".fill-box").fillBox();
-			}
+			// else if (img.search("imgur") != -1) {
+			// 	console.log(img)
+			// 	img = img+".jpg"
+			// 	$('#images').append('<a href="' + img + '"><div class="image"><img class="fill-box" src="' + img + '"/></div></a>');
+			// 	$(".fill-box").fillBox();
+			// }
 	    });
 	});
 };
+
